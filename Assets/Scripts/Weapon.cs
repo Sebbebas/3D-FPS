b5c6cf4239df70ffef344ffe90645330c32ec2bf
Sebastian Nuñez Larsson <Sebbebas1005@gmail.com>
2024-04-28 12:38:25 +0200

    Commit 5

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Weapon : MonoBehaviour
 {
-    //Configurabel Perameters
+    //Configurable Perameters
     [Header("Type")]
     public WeaponState WeaponType = WeaponState.Total;
 
@@ -12,24 +13,30 @@ public class Weapon : MonoBehaviour
     public int currentAmmo;
     public int totalAmmo = 90;
     public int magSize = 30;
+    public TextMeshProUGUI ammoText;
 
     [Header("Extra")]
     public LayerMask ignoreHitMask = 0;
     public float weaponRange = 100f;
-    public float reloadTime = 3;
     protected Camera mainCam = null;
+
+    [Header("Delays")]
+    public float reloadTime = 3f;
+    public float firedelay = 3f;
 
     [Header("Effects")]
     public ParticleSystem hitParticle;
     public Animator animator;
 
     //Private Variabels
+    private float currentFireTime;
+    private float currentReloadTime;
 
     protected void Start()
     {
         mainCam = Camera.main;
 
-        if(currentAmmo == 0)
+        if (currentAmmo == 0)
         {
             currentAmmo = magSize;
         }
@@ -37,17 +44,36 @@ public class Weapon : MonoBehaviour
 
     private void Update()
     {
-        
+        //Reload
+        if (currentAmmo == 0)
+        {
+            Reload();
+        }
+
+        if (gameObject.activeSelf) { ammoText.text = currentAmmo.ToString() + "/" + totalAmmo.ToString(); }
+
+        //Cooldowns
+        if (currentFireTime > 0) { currentFireTime -= Time.deltaTime; }
+        else { currentFireTime = 0; if (animator != null) { animator.SetBool("Fire", false); } }
+
+        if (currentReloadTime > 0) { currentReloadTime -= Time.deltaTime; }
+        else { currentReloadTime = 0; if (animator != null) { animator.SetBool("Reload", false); } }
     }
 
     public void Reload()
     {
         if (currentAmmo == magSize && totalAmmo == 0) { Debug.Log("cant reload"); return; }
+        if (animator != null) { animator.SetBool("Reload", true); }
 
-        animator.SetBool("Reload", true);
+        StartCoroutine(ReloadRoutine());
+        currentReloadTime = reloadTime;
+    }
 
+    public IEnumerator ReloadRoutine()
+    {
+        yield return new WaitForSeconds(reloadTime);
         int ammoToAdd = magSize - currentAmmo;
-        
+
         if (totalAmmo >= ammoToAdd)
         {
             currentAmmo = magSize;
@@ -58,24 +84,25 @@ public class Weapon : MonoBehaviour
             currentAmmo += currentAmmo;
             currentAmmo = 0;
         }
+
+        StopCoroutine(ReloadRoutine());
     }
 
     public virtual bool Fire()
     {
         //false
-        if(currentAmmo < 1)
+        if (currentAmmo == 0 || currentFireTime > 0 && currentAmmo != 0 || currentReloadTime > 0)
         {
+            if (currentAmmo == 0)
+            {
+                Reload();
+            }
             return false;
         }
         //true
         currentAmmo--;
-        animator.SetBool("Fire", true);
-
-        //no ammo left in mag
-        if (currentAmmo == 0)
-        {
-            Reload();
-        }
+        currentFireTime = firedelay;
+        if (animator != null) { animator.SetBool("Fire", true); } //Null Check can be simplified
         return true;
     }
 }
